d69b54fd9066208135cc4d3f27cde3f7d5fbd22d
Minsub Ji <jiminsub@gmail.com>
2014-02-24 17:08:48 +0900

    test

--- JavaTest/src/com/minsub/dp/singleton/Main.java
+++ JavaTest/src/com/minsub/dp/singleton/Main.java
@@ -14,6 +14,9 @@ public class Main {
 		Singleton s2 = Singleton.getInstance();
 		System.out.println("s2.a is " + s2.getA());
 		
+		
+		s2.setA(5);
+		System.out.println("s.a is " + s.getA());
 	}
 
 }
